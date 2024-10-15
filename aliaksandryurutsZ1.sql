5f1e2e94eb109c8e9d4fc8a60d8bb44c29ad846a
YurutsAliaksandr <162021937+YurutsAliaksandr@users.noreply.github.com>
2024-10-15 12:05:27 +0300

    Create aliaksandryurutsZ1.sql

---
+++ aliaksandryurutsZ1.sql
@@ -0,0 +1,5 @@
+CREATE USER 'programista'@'localhost' IDENTIFIED BY 'ZsIpOnTwL2024';
+CREATE USER 'artur'@'localhost' IDENTIFIED BY '4202LwTnOpIsZ';
+CREATE USER 'agnieszka'@'%' IDENTIFIED BY 'cYbErKnUp4920';
+DROP USER 'artur'@'localhost';
+SELECT user, host FROM mysql.user;
